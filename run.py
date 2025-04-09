343c283354d82526e34048ae6b170cd0cf43edcc
Evgen <zhenyagolenko92@gmail.com>
2025-04-09 15:29:24 +0300

    ID, help, photo

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
 
@@ -14,7 +14,24 @@ async def main():
 
 @dp.message(CommandStart())
 async def cmd_start(message:Message):
-    await message.answer('Привет')
+    await message.reply(f'Привет.\nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}')
+
+@dp.message(Command('help'))
+async def get_help(message:Message):
+    await message.answer('Это команда /help')
+
+@dp.message(F.text == 'Как дела?')
+async def how_are_you(message:Message):
+    await message.answer("OK!")
+
+@dp.message(Command('get_photo'))
+async def get_photo(message:Message):
+    await message.answer_photo(photo='https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D0%B9%D0%BB:Telegram_Messenger.png',
+                               caption='Это лого ТГ')
+
+@dp.message(F.photo)
+async def get_photo(message:Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
