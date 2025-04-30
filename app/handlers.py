eed721771f1605f65e48a9a73a74337cea34b8bb
Evgen <zhenyagolenko92@gmail.com>
2025-05-01 00:47:58 +0300

    middleware

--- app/handlers.py
+++ app/handlers.py
@@ -4,12 +4,16 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 import app.keyboards as kb
+from app.middlewares import TestMiddleware
+
 
 router = Router()
+router.message.outer_middleware(TestMiddleware())
 
 class Reg(StatesGroup):
     name = State()
     number = State()
+
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -37,24 +41,28 @@ async def get_photo(message: Message):
 async def get_photo(message: Message):
     await message.answer(f'ID фото: {message.photo[-1].file_id}')
 
+
 @router.callback_query(F.data == 'catalog')
-async def catalog(callback:CallbackQuery):
+async def catalog(callback: CallbackQuery):
     await callback.answer('Вы выбрали каталог', show_alert=True)
     await callback.message.edit_text('Привет', reply_markup=await kb.inline_confectionery())
 
+
 @router.message(Command('reg'))
-async def reg_one(message:Message, state: FSMContext):
+async def reg_one(message: Message, state: FSMContext):
     await state.set_state(Reg.name)
     await message.answer('Введите свое имя:')
 
+
 @router.message(Reg.name)
-async def reg_two(message:Message, state: FSMContext):
+async def reg_two(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(Reg.number)
     await message.answer('Введите номер телефона:')
 
+
 @router.message(Reg.number)
-async def two_three(message:Message, state: FSMContext):
+async def two_three(message: Message, state: FSMContext):
     await state.update_data(number=message.text)
     data = await state.get_data()
     await message.answer(f'Спасибо, регистрация завершена.\nИмя: {data["name"]}\nНомер: {data["number"]}')
